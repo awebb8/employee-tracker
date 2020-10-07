6765c5cde18146778c57a8772b2d587dcb7230dd
Alkawe <alixkwebb@gmail.com>
2020-10-06 23:54:13 -0400

    updated tables

--- database/schema.sql
+++ database/schema.sql
@@ -4,25 +4,26 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-	id INT NOT NULL,
-    name VARCHAR(30) NULL,
+	id INT AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR(30) NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-	id INT NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-	id INT NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+	role_id INT NOT NULL,
     manager_id INT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (manager_id)
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
--- database/seed.sql
+++ database/seed.sql
@@ -1,26 +1,23 @@
--- Add content to the department table
-INSERT INTO department (id, name) VALUES 
-(73, "Human Resources"),
-(83, "Accounting"),
-(39, "Marketing"),
-(25, "Research and Development");
-
+INSERT INTO department (dept_name) VALUES 
+("Human Resources"),
+("Accounting"),
+("Marketing"),
+("Research and Development");
 
 -- Add content to the role table
-INSERT INTO role (id, title, salary, department_id) VALUES
-(735923, "HR Manager", 78000.00, 73),
-(836922, "Accountant", 67000.00, 83),
-(394774, "Director of Marketing", 108000.00, 39),
-(397252, "Marketing Analyst", 56000.00, 39),
-(253263, "Director of Engineering", 152000.00, 25),
-(258394, "Product Engineer", 92000.00, 25);
-
+INSERT INTO role (title, salary, department_id) VALUES
+("HR Manager", 78000.00, 1),
+("Accountant", 67000.00, 2),
+("Director of Marketing", 108000.00, 3),
+("Marketing Analyst", 56000.00, 3),
+("Director of Engineering", 152000.00, 4),
+("Product Engineer", 92000.00, 4);
 
 -- Add content to the employee table
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
-(735923, "Timothy", "Williams", 731, NULL),
-(836922, "Amber", "Smith", 835, NULL),
-(394774, "Jamie", "Miller", 391, NULL),
-(397252, "Andrew", "Phillips", 397, 394774),
-(253263, "Laurel", "Davis", 251, NULL),
-(258394, "Joseph", "Johnson", 253, 253263);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+("Timothy", "Williams", 1, NULL),
+("Amber", "Smith", 2, NULL),
+("Jamie", "Miller", 3, NULL),
+("Andrew", "Phillips", 4, 3),
+("Laurel", "Davis", 5, NULL),
+("Joseph", "Johnson", 6, 5);
